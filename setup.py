f3f40c261d0551e3b42a762af8d9d32d8a914406
zhengchenyu <zcy19941015@qq.com>
2017-03-06 14:40:07 +0800

    setup.py 0.0.2 and edit project URL

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="pycreeper",
-    version="0.0.1",
+    version="0.0.2",
     description='''Creepers feel like a web crawler:
                     slow growth and and wide coverage.''',
     author="zcy",
     author_email="zhengchenyu.backend@gmail.com",
-    url="http://blog.csdn.net/zcy19941015",
+    url="https://github.com/ZcyAndWt/pyCreeper",
     license="LGPL",
     packages=find_packages(exclude=('tests', 'tests.*', 'examples', 'examples.*')),
     install_requires=[
