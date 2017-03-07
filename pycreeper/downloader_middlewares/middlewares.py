23071a05e782c804dacf1c8c6dee0c1673c197e6
zcy <zcy19941015@qq.com>
2017-03-07 23:25:49 +0800

    add Cookie Middlewares

--- pycreeper/downloader_middlewares/middlewares.py
+++ pycreeper/downloader_middlewares/middlewares.py
@@ -9,6 +9,12 @@ from logging import Logger
 import chardet
 import gevent
 from pycreeper.downloader_middlewares import DownloaderMiddleware
+import os
+import six
+from collections import defaultdict
+
+from pycreeper.http.response import Response
+from pycreeper.http.cookies import CookieJar
 
 
 class RetryMiddleware(DownloaderMiddleware):
@@ -122,3 +128,61 @@ class EncodingDiscriminateMiddleware(DownloaderMiddleware):
             self.ENCODING_MAP[netloc] = encoding
         body = content.decode(self.ENCODING_MAP[netloc], "replace")
         return response.copy(body=body)
+
+
+class CookiesMiddleware(object):
+    """This middleware enables working with sites that need cookies"""
+
+    def __init__(self, settings, logger):
+        self.jars = defaultdict(CookieJar)
+        self.settings = settings
+        self.logger = logger
+
+    def process_request(self, request):
+        if request.meta.get('dont_merge_cookies', False):
+            return
+
+        cookiejarkey = request.meta.get("cookiejar")
+        jar = self.jars[cookiejarkey]
+        cookies = self._get_request_cookies(jar, request)
+        for cookie in cookies:
+            jar.set_cookie_if_ok(cookie, request)
+
+        # set Cookie header
+        request.headers.pop('Cookie', None)
+        jar.add_cookie_header(request)
+
+    def _get_request_cookies(self, jar, request):
+        if isinstance(request.cookies, dict):
+            cookie_list = [{'name': k, 'value': v}
+                           for k, v in six.iteritems(request.cookies)]
+        else:
+            cookie_list = request.cookies
+
+        cookies = [self._format_cookie(x) for x in cookie_list]
+        headers = {'Set-Cookie': cookies}
+        response = Response(request.url, headers=headers)
+
+        return jar.make_cookies(response, request)
+
+    def process_response(self, request, response, spider):
+        if request.meta.get('dont_merge_cookies', False):
+            return response
+
+        # extract cookies from Set-Cookie and drop invalid/expired cookies
+        cookiejarkey = request.meta.get("cookiejar")
+        jar = self.jars[cookiejarkey]
+        jar.extract_cookies(response, request)
+
+        return response
+
+    def _format_cookie(self, cookie):
+        # build cookie string
+        cookie_str = '%s=%s' % (cookie['name'], cookie['value'])
+
+        if cookie.get('path', None):
+            cookie_str += '; Path=%s' % cookie['path']
+        if cookie.get('domain', None):
+            cookie_str += '; Domain=%s' % cookie['domain']
+
+        return cookie_str
