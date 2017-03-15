94ee0bb026091a9f41c5ee9c783e1ff6a9c55c56
zcy <zcy19941015@qq.com>
2017-03-15 22:54:26 +0800

    动态请求返回加cookiejar

--- pycreeper/downloader/__init__.py
+++ pycreeper/downloader/__init__.py
@@ -10,6 +10,7 @@ from pycreeper.downloader_middlewares import DownloaderMiddlewareManager
 from selenium.common.exceptions import TimeoutException as _TimeoutException
 from pycreeper.utils.exceptions import TimeoutException
 from requests.exceptions import Timeout
+import six
 from pycreeper.http.headers import Headers
 from urlparse import urlparse
 
@@ -75,14 +76,16 @@ class DownloadHandler(object):
     def _fetch_dynamic(self, request, url):
         try:
             self.driver_sem.acquire()
-            cookies = request.headers.get_cookies()
-            self._removed_first_dot_in_front_of_domain(cookies)
-            command_list = self._get_command_list(cookies)
-            # make the current page to have the same domain with cookies
-            self.driver.get(url)
-            # load cookies
-            for command in command_list:
-                self.driver.execute_script(command)
+            if request.cookiejar:
+                cookies = self._get_cookies_from_cookiejar(request.cookiejar)
+                cookies = self._covert_cookies_to_dict(cookies)
+                #self._removed_first_dot_in_front_of_domain(cookies)
+                command_list = self._get_command_list(cookies)
+                # make the current page to have the same domain with cookies
+                self.driver.get(url)
+                # load cookies
+                for command in command_list:
+                    self.driver.execute_script(command)
 
             self.driver.set_page_load_timeout(self.settings["TIMEOUT"])
             self.driver.implicitly_wait(request.wait)
@@ -90,11 +93,20 @@ class DownloadHandler(object):
             for func in request.browser_actions:
                 func(self.driver)
             html = self.driver.page_source
-            all_cookies = self.driver.get_cookies()
 
+            # generate cookies
+            all_cookies = self.driver.get_cookies()
             self.driver.delete_all_cookies()
             self.driver_sem.release()
-            return Response(url, request, body=html)
+
+            all_cookies = self._to_byte(all_cookies)
+            cookies = [self._make_cookie(**d) for d in all_cookies]
+
+            # set cookies to cookiejar
+            cj = cookielib.CookieJar()
+            for cookie in cookies:
+                cj.set_cookie(cookie)
+            return Response(url, request, cookiejar=cj, body=html)
         except _TimeoutException as e:
             raise TimeoutException(e.message)
 
@@ -122,25 +134,47 @@ class DownloadHandler(object):
                     result.append(cookie)
         return result
 
-    def _make_cookie(self, name, value):
+    def _make_cookie(self, **kwargs):
         return cookielib.Cookie(
             version=0,
-            name=name,
-            value=value,
+            name=kwargs.get('name', None),
+            value=kwargs.get('value', None),
             port=None,
             port_specified=False,
-            domain="court.gov.cn",
+            domain=kwargs.get('domain', None),
             domain_specified=True,
             domain_initial_dot=False,
-            path="/",
+            path=kwargs.get('path', None),
             path_specified=True,
             secure=False,
-            expires=None,
+            expires=kwargs.get('expires', None),
             discard=False,
             comment=None,
             comment_url=None,
             rest=None
         )
+
+    def _covert_cookies_to_dict(self, cookies):
+        result = []
+        for cookie in cookies:
+            cookie_dict = {}
+            for key in ['name', 'value', 'domain', 'path', 'expiry']:
+                if getattr(cookie, key):
+                    cookie_dict[key] = getattr(cookie, key)
+            result.append(cookie_dict)
+        return result
+
+    def _to_byte(self, cookies):
+        result = []
+        for cookie in cookies:
+            temp = {}
+            for key in cookie.keys():
+                temp[key.encode('utf-8') if isinstance(key, six.text_type) else key] = \
+                    cookie[key].encode('utf-8') if isinstance(cookie[key], six.text_type) else cookie[key]
+            result.append(temp)
+        return result
+
+
 
 
 class Downloader(object):
